59cae4aa6acae9b973e8405c77e660b818b49f94
Kevin <kevinrmann.com>
2013-09-18 21:30:16 -0600

    Building out models

--- apps/login/models.py
+++ apps/login/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ZombieUser(models.Model):
+	id = models.AutoField(primary_key=True)
+	user = models.OneToOneField(User)
+	gamesWon = models.IntegerField(null=True, blank=True)
+	gamesLost = models.IntegerField(null=True, blank=True)
+	accountType = models.IntegerField(null=True, blank=True)
+	maps = models.ManyToManyField('Maps', null=True, blank=True)
+
+	def __unicode__(self):
+		return unicode(self.user)
+
+class Maps(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=60)
+	owner = models.ForeignKey('ZombieUser', related_name='map_owner')
+
+	def __unicode__(self):
+		return unicode(self.name)
+
+
+
--- settings.py
+++ settings.py
@@ -122,10 +122,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    # Custom apps
+    'zombie.apps.login',
+    # Django admin
+    'django.contrib.admin',
 )
 
 # A sample logging configuration. The only tangible logging
