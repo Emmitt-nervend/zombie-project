5660c0f52bb982de8ef21f365ed132848a2bda66
Russell Maxfield <Russ.Max783@gmail.com>
2013-09-19 21:03:35 -0600

    database config

--- urls.py
+++ urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import patterns, include, url
 from zombie.apps.login.views import home
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'zombie.apps.login.views.home', name='home'),
+
+    #for admin
+    url(r'^admin/', include(admin.site.urls)),
 )
