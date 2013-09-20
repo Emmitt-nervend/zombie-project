e6d12a780dd29871c6b759cd58d3962be1c506fd
Kevin <kevinrmann.com>
2013-09-19 22:04:04 -0600

    Adding login view

--- apps/login/views.py
+++ apps/login/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, render_to_response
 
 def home(request):
 	return render(request, 'home.html', {})
+
+def login(request):
+	return render(request, 'login.html', {})
--- urls.py
+++ urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from zombie.apps.login.views import home
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'zombie.apps.login.views.home', name='home'),
+    url(r'^login/$', 'zombie.apps.login.views.login', name='login'),
     url(r'^admin/', include(admin.site.urls)),
 )
