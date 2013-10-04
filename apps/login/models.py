38fa7872a29c7f005e1309d5a1bbd5f349522653
Kevin Mann <kevinrmann@gmail.com>
2013-10-03 20:10:40 -0600

    Changing map model

--- apps/login/models.py
+++ apps/login/models.py
@@ -15,11 +15,15 @@ class ZombieUser(models.Model):
 
 class Map(models.Model):
 	id = models.AutoField(primary_key=True)
-	name = models.CharField(max_length=60)
+	title = models.CharField(max_length=60)
 	owner = models.ForeignKey('ZombieUser', related_name='map_owner')
+	width = models.IntegerField()
+	height = models.IntegerField()
+	x = models.IntegerField()
+	y = models.IntegerField()
+	data = models.CharField(max_length=5000)
+	events = models.CharField(max_length=1000)
+	environment = models.CharField(max_length=100)
 
 	def __unicode__(self):
 		return unicode(self.name)
-
-
-
