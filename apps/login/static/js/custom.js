761e5a7f85b21706f2f9a4a86184de4cbf97660c
Kevin Mann <kevinrmann@gmail.com>
2013-10-07 14:47:01 -0600

    Fixing top right rotation of logo

--- apps/login/static/js/custom.js
+++ apps/login/static/js/custom.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+
 	$(".top-right img").mouseover(function(){
 		$(".top-right-form").show("slide", {direction: "right"});
-
+		$(".top-right img").addClass("top-right-rotate");
 	});
 	$(".top-right-form").mouseleave(function(){
 		$(".top-right-form").hide("slide", {direction: "right"});
+		$(".top-right img").toggleClass("top-right-rotate");
+
 	});
 	$("#password").keyup(function(e){
 		if (e.keyCode == 13) {
