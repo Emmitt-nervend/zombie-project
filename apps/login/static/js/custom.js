08d65ca03f95edd1ec6e0b56354dab7d02ac1945
Kevin Mann <kevinrmann@gmail.com>
2013-10-09 15:03:35 -0600

    Fixing spinny thing

--- apps/login/static/js/custom.js
+++ apps/login/static/js/custom.js
@@ -1,23 +1,33 @@
 $(document).ready(function(){
+
 	$(".top-right img").click(function(){
 		$(".top-right-form").show("slide", {direction: "right"});
 		$(".top-right img").toggleClass("top-right-rotate");
 		$(".top-right-form").toggle("slide", {direction: "right"});
 	});
+
 	$(".top-right img").mouseover(function(){
 		$(".top-right-form").show("slide", {direction: "right"});
+		$(".top-right-panel").show("slide", {direction: "right"});
 		$(".top-right img").addClass("top-right-rotate");
 	});
+
 	$(".top-right-form").mouseleave(function(){
 		$(".top-right-form").hide("slide", {direction: "right"});
 		$(".top-right img").toggleClass("top-right-rotate");
-
 	});
+
+	$(".top-right-panel").mouseleave(function(){
+		$(".top-right-panel").hide("slide", {direction: "right"});
+		$(".top-right img").toggleClass("top-right-rotate");
+	});
+
 	$("#password").keyup(function(e){
 		if (e.keyCode == 13) {
 			this.form.submit();
 		}
-	})
+	});
+	
 	$("#signIn").click(function(){
 		$(".top-right-form").show("slide", {direction: "right"});
 		$(".top-right img").addClass("top-right-rotate");
