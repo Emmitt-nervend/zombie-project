e525d60bb87f231a85d4a114334706d850e46875
Bryce <bryce@bryce-Q430-Q530.(none)>
2013-11-06 22:46:40 -0700

    Major map Editor Updates

--- apps/web/static/js/app/views/editor.js
+++ apps/web/static/js/app/views/editor.js
@@ -11,21 +11,50 @@ define([
     Handlebars,
     template
 ) {
-    var SIZE = 40;
-    var NUM_TILES = 44;
-    var COLS = 8;
-    var SRC = '/static/images/bottom.png';
-    var selectedLeft = 0;
-    var selectedRight = 0;
-
+    
     var EditorView = Backbone.View.extend({
 
         template: Handlebars.compile(template),
 
         initialize: function() {
-            var self = this;
+            
             this.constructor.__super__.initialize.apply(this, [this.options]);
-            this.render();
+            this.SIZE = 40;
+            this.NUM_TILES_BOTTOM = 44;
+            this.NUM_TILES_MIIDLE = 72;
+            this.NUM_TILES_UPPER = 8;
+            this.NUM_TILES_EVENTS = 8;
+            this.COLS = 8;
+            this.EditorColums = 8;
+            this.EditorRows = 8; 
+            this.SRCBOTTOM = '/static/images/bottom.png';
+            this.SRCEVENTS = '/static/images/events.png';
+            this.SRCMIDDLE = '/static/images/middle.png';
+            this.SRCTOP = '/static/images/upper.png';
+            this.selectedLeft = 0;
+            this.selectedRight = 0;
+            this.showGrid = true;
+            this.jsonMapObject = { 
+                "title": "",
+                "author": "",  
+                "width": 0,   
+                "height": 0,  
+                "x": 0,       
+                "y": 0,       
+                "events": [Event], 
+                "data": {
+                    "bottom": [],  
+                    "middle": [],  
+                    "top": [],     
+                },
+                "env": ""    
+                };  
+            for(i = 0; i < this.COLS; i++)
+            {
+              this.jsonMapObject.data.top[i] = [];
+              this.jsonMapObject.data.middle[i] = [];
+              this.jsonMapObject.data.bottom[i] = [];
+            }
         },
 
         destroy: function() {
@@ -35,123 +64,207 @@ define([
         events: {
             'mousedown .tile':'tileClick',
             'mousedown canvas':'drawTiletoMap',
-            'click #toggle': 'toggleGrid'
+            'click #toggle': 'toggleGrid',
+            'click #saveMap': 'saveMapToServer',
+            'click .tileSetSwitch': 'switchTiles'
         },
 
         render: function() {
         
             this.$el.empty().html(this.template({
             }));
-            this.buildMapEditor()
+            this.buildMapEditor();
+            var that = this;
+            setTimeout(function(){
+                for (var i = 0; i < 8; ++i) {
+                    for (var j = 0; j < 8; ++j) {
+                        //console.log(i+ ", "+j);
+                        that.drawPiece(22, i, j);
+                        //console.log(this.jsonMapObject);
+                    }
+                };
+            },100);
             return this;
-<<<<<<< HEAD
-=======
-
->>>>>>> 39c2e8fb414090db5a48b8f82bbf753cd590a675
         },
         /*non-event functions*/
-        addTile: function(i){
-              var tiles = this.$('#tiles');
+        addTile: function(i, source, key){
+            if(key==="bottom")
+            {
+                var tiles = this.$('#tilesBottom');
+            }
+            if(key==="middle")
+            {
+                var tiles = this.$('#tilesMiddle');
+            }
+            if(key==="top")
+            {
+                var tiles = this.$('#tilesTop');
+            }
+            if(key==="events")
+            {
+                var tiles = this.$('#tilesEvents');
+            }
 
-              var tile = $('<div>');
-              tile.attr('id', i);
-              tile.addClass('tile');
+            var tile = $('<div>');
+            tile.attr('id', i);
+            tile.addClass('tile');
 
-              var xOffset = -i % COLS * SIZE;
-              var yOffset = -Math.floor(i / COLS) * SIZE;
+            var xOffset = -i % this.COLS * this.SIZE;
+            var yOffset = -Math.floor(i / this.COLS) * this.SIZE;
+            tile.css({
+                width: this.SIZE + 'px',
+                height: this.SIZE + 'px',
+                background: 'url(' + source + ') ' + xOffset + 'px ' + yOffset +'px'
+            });
+            if (i === this.selectedLeft || i===this.selectedRight) {
+                tile.addClass('selected');
+            }
 
-              tile.css({
-                width: SIZE + 'px',
-                height: SIZE + 'px',
-                background: 'url(' + SRC + ') ' + xOffset + 'px ' + yOffset +'px'
-              });
-                if (i === selectedLeft || i===selectedRight) {
-                  tile.addClass('selected');
-                }
-
-              tiles.append(tile);
-              if(i===38)
-              {
+            tiles.append(tile);
+            if(i===38 && key==="bottom")
+            {
                 tile.hide();
-              }
+            }
+            if(i>=5 && key==="events")
+            {
+                tile.hide();
+            }
         },
         drawPiece:function(id, x, y) {
-          var ctx = this.$('#map')[0].getContext('2d');
-          var img=document.createElement('image');
-          img.src=SRC;
-          var xOffset = id % COLS * SIZE;
-          var yOffset = Math.floor(id / COLS) * SIZE;
+            var tileSet = this.$('.displayed').attr('id');
+            if (!tileSet)
+            {
+                tileSet = "tilesBottom";
+            }
 
-          ctx.drawImage(img, xOffset, yOffset, SIZE, SIZE, x * SIZE, y * SIZE, SIZE, SIZE);
+            if(tileSet =="tilesBottom")
+            {
+                var ctx = this.$('#mapBottom')[0].getContext('2d');
+                var source = this.SRCBOTTOM;
+                this.jsonMapObject.data.bottom[y][x]=parseInt(id);
+            }
+            else if(tileSet =="tilesMiddle")
+            {
+                var ctx = this.$('#mapMiddle')[0].getContext('2d');
+                var source = this.SRCMIDDLE;
+                this.jsonMapObject.data.middle[y][x]=parseInt(id);
+            }
+            else if(tileSet =="tilesTop")
+            {
+                var ctx = this.$('#mapTop')[0].getContext('2d');
+                var source = this.SRCTOP;
+                this.jsonMapObject.data.top[y][x]=parseInt(id);
+            }
+            else if(tileSet =="tilesEvents")
+            {
+                var ctx = this.$('#mapEvents')[0].getContext('2d');
+                var source = this.SRCEVENTS;
+                //this.jsonMapObject.data.events=parseInt(id);
+            }
+            var img=document.createElement('image');
+            img.src=source;
+            var xOffset = id % this.COLS * this.SIZE;
+            var yOffset = Math.floor(id / this.COLS) * this.SIZE;
+            this.jsonMapObject.data.bottom[y][x]=parseInt(id);
+            ctx.drawImage(img, xOffset, yOffset, this.SIZE, this.SIZE, x * this.SIZE, y * this.SIZE, this.SIZE, this.SIZE);
         },
         /*Event Functions*/
         tileClick: function(e){
-          if (e.which == 1)
-            selectedLeft = e.target.getAttribute('id');
-          if (e.which == 3)
-            selectedRight = e.target.getAttribute('id');
-
-          $('.selected').removeClass('selected');
-          $('#' + selectedLeft).addClass('selected');
-          $('#' + selectedRight).addClass('selected');
+            if (e.which == 1)
+                this.selectedLeft = e.target.getAttribute('id');
+            if (e.which == 3)
+                this.selectedRight = e.target.getAttribute('id');
+            $('.selected').removeClass('selected');
+            $('#' + this.selectedLeft).addClass('selected');
+            $('#' + this.selectedRight).addClass('selected');
         },
         buildMapEditor: function(){
-          var clickedLeft = false;
-          var clickedRight = false;
-          var showGrid = true;
-          var grid = this.$('.grid')[0].getContext('2d');
-          var img = new Image();
-          img.src = SRC;
-          var self = this;
-          /*img.onload = function () {
-              $(document).mousedown(function (e) {
-                    if (e.which === 1) clickedLeft = true;
-                    if (e.which === 3) clickedRight = true;
-                  });
-              $(document).mouseup(function (e) {
-                    if (e.which === 1) clickedLeft = false;
-                    if (e.which === 3) clickedRight = false;
-                  });
-              self.$('#grid').mousemove(function (e) {
-                    var x = Math.floor(e.offsetX / SIZE);
-                    var y = Math.floor(e.offsetY / SIZE);
-
-                    if (clickedLeft)
-                      self.drawPiece(selectedLeft, x, y, this);
-                    if (clickedRight)
-                      self.drawPiece(selectedRight, x, y, this);
-              });
-            }*/
-          /*for loops outside functions*/
-            for (var i = 0; i < NUM_TILES; ++i) {
-              this.addTile(i);
+            var clickedLeft = false;
+            var clickedRight = false;
+            var grid = this.$('.grid')[0].getContext('2d');
+            var tileCount;
+            var keys =["bottom", "middle", "top", "events"];
+            for(var index in keys)
+            {
+                if(keys[index]==="bottom")
+                {
+                    source=this.SRCBOTTOM;
+                    tileCount=this.NUM_TILES_BOTTOM;
+                }
+                else if(keys[index]==="middle")
+                {
+                    source=this.SRCMIDDLE;
+                    tileCount=this.NUM_TILES_MIIDLE;
+                }
+                else if(keys[index]==="top")
+                {
+                    source=this.SRCTOP;
+                    tileCount=this.NUM_TILES_UPPER;
+                }
+                else if(keys[index]==="events")
+                {
+                    source=this.SRCEVENTS;
+                    tileCount=this.NUM_TILES_EVENTS;
+                }
+                for (var i = 0; i < tileCount; ++i) {
+                    this.addTile(i, source, keys[index]);
+                };
+            }
+            for (var i = 0; i < this.EditorColums; ++i) {
+                for (var j = 0; j < this.EditorRows; ++j) {
+                    grid.strokeRect(i*this.SIZE, j*this.SIZE, this.SIZE, this.SIZE);
+                }
             };
-            for (var i = 0; i < 8; ++i) {
-              for (var j = 0; j < 8; ++j) {
-                grid.strokeRect(i*SIZE, j*SIZE, SIZE, SIZE);
-              }
-            };
+            this.$('.tileSet').hide();
+            this.$('.displayed').show();
             
         },
-      drawTiletoMap: function(e){
-          var clickedRight = false;
-          var clickedLeft = false;
-          if (e.which === 1) clickedLeft = true;
-          if (e.which === 3) clickedRight = true;
-          var x = Math.floor(e.offsetX / SIZE);
-          var y = Math.floor(e.offsetY / SIZE);
-          if (clickedLeft)
-              this.drawPiece(selectedLeft, x, y);
-          else if (clickedRight)
-              this.drawPiece(selectedRight, x, y);
-      },
-      toggleGrid: function(){
-         this.showGrid = !this.showGrid;
+        drawTiletoMap: function(e){
+            var clickedRight = false;
+            var clickedLeft = false;
+            if (e.which === 1) clickedLeft = true;
+            if (e.which === 3) clickedRight = true;
+            var x = Math.floor(e.offsetX / this.SIZE);
+            var y = Math.floor(e.offsetY / this.SIZE);
+            if (clickedLeft)
+                this.drawPiece(this.selectedLeft, x, y);
+            else if (clickedRight)
+                this.drawPiece(this.selectedRight, x, y);
+        },
+        toggleGrid: function(){
+            this.showGrid = !this.showGrid;
+            var func = this.showGrid ? 'removeClass' : 'addClass';
+            $('#grid')[func]('hidden');
+        },
+        saveMapToServer: function(){
+            this.jsonMapObject.title = $('#mapTitle').val();
+            this.jsonMapObject.author=USER;
+            this.jsonMapObject.width = parseInt($('#mapBottom').attr('width'))/40;
+            this.jsonMapObject.height = parseInt($('#mapBottom').attr('height'))/40;
+            console.log(JSON.stringify(this.jsonMapObject));
+        },
+        switchTiles: function(e){
+            var showSet = e.target.id;
+            this.$('.displayed').hide().removeClass('displayed');
+            if(showSet =="showBottomTiles")
+            {
+                this.$("#tilesBottom").addClass('displayed')
+            }
+            else if(showSet =="showMiddleTiles")
+            {
+                this.$("#tilesMiddle").addClass('displayed')
+            }
+            else if(showSet =="showTopTiles")
+            {
+                this.$("#tilesTop").addClass('displayed')
+            }
+            else if(showSet =="showEventTiles")
+            {
+                this.$("#tilesEvents").addClass('displayed')
+            }
+            this.$('.displayed').show();
+        }
 
-        var func = this.showGrid ? 'removeClass' : 'addClass';
-
-        $('#grid')[func]('hidden');
-      }
     });
     return EditorView;
 });
