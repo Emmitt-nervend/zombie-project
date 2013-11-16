f89860aef49eeb889b774e6c01c62e3c63829f14
Kevin Mann <kevinrmann@gmail.com>
2013-11-16 10:51:11 -0700

    Making the events save as json... hopefully

--- apps/rest/views.py
+++ apps/rest/views.py
@@ -101,11 +101,10 @@ class SaveMap(generics.GenericAPIView):
 		url = 'http://zombie-attack.aws.af.cm/uploadMap/ae8c7e77-4e02-4d95-a63a-603b44cadf87'
 		headers = {'content-type': 'application/json'}
 		map_dict = json.loads(request.POST['map'])
-		print(type(map_dict['events']))
-		print(map_dict['events'])
 		if map_dict['events'] == '[None]':
-			print('here')
 			map_dict['events'] = []
+		else:
+			map_dict['events'] = json.dumps(map_dict['events'])
 		json_map = json.dumps({'map':map_dict})
 		r = requests.post(url, data=json_map, headers=headers)
 		response_dict = json.loads(r.text)
